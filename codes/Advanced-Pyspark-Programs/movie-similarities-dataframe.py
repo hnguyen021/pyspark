1ecb5fc3022c390ae0c51e363ca54554fb7fd138
hnguyen021 <135601813+hnguyen021@users.noreply.github.com>
2023-10-21 12:00:53 +0700

    Update movie-similarities-dataframe.py

--- codes/Advanced-Pyspark-Programs/movie-similarities-dataframe.py
+++ codes/Advanced-Pyspark-Programs/movie-similarities-dataframe.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
 import sys
+from pyspark.sql.functions import col
 
 def computeCosineSimilarity(spark, data):
     # Compute xx, xy and yy columns
@@ -56,13 +57,13 @@ movieNames = spark.read \
       .option("sep", "|") \
       .option("charset", "ISO-8859-1") \
       .schema(movieNamesSchema) \
-      .csv("file:///SparkCourse/ml-100k/u.item")
-
+      .csv("../../datasets/ml-100k/u.item")
+#movieNames.show()
 # Load up movie data as dataset
 movies = spark.read \
       .option("sep", "\t") \
       .schema(moviesSchema) \
-      .csv("file:///SparkCourse/ml-100k/u.data")
+      .csv("../../datasets/ml-100k/u.data")
 
 
 ratings = movies.select("userId", "movieId", "rating")
@@ -70,14 +71,22 @@ ratings = movies.select("userId", "movieId", "rating")
 # Emit every movie rated together by the same user.
 # Self-join to find every combination.
 # Select movie pairs and rating pairs
-moviePairs = ratings.alias("ratings1") \
-      .join(ratings.alias("ratings2"), (func.col("ratings1.userId") == func.col("ratings2.userId")) \
-            & (func.col("ratings1.movieId") < func.col("ratings2.movieId"))) \
-      .select(func.col("ratings1.movieId").alias("movie1"), \
-        func.col("ratings2.movieId").alias("movie2"), \
-        func.col("ratings1.rating").alias("rating1"), \
-        func.col("ratings2.rating").alias("rating2"))
-
+rating1 = ratings.alias("ratings1")
+rating2 = ratings.alias("ratings2")
+joinConditions = (func.col("ratings1.userId") == func.col("ratings2.userId")) & (func.col("ratings1.movieId") < func.col("ratings2.movieId"))
+moviePairs = rating1.join(rating2,joinConditions).select(func.col("ratings1.movieId").alias("movie1"),\
+                                                         func.col("ratings2.movieId").alias("movie2"),\
+                                                         func.col("ratings1.rating").alias("rating1"),\
+                                                         func.col("ratings2.rating").alias("rating2"))
+#joinConditions = (func.col("ratings1.userId") == func.col("ratings2.userId")) & (func.col("ratings1.movieId") < func.col("ratings2.movieId"))
+# moviePairs = ratings.alias("ratings1") \
+#       .join(ratings.alias("ratings2"),(func.col("ratings1.userId") == func.col("ratings2.userId"))\
+#            & (func.col("ratings1.movieId") < func.col("ratings2.movieId")) ) \
+#       .select(func.col("ratings1.movieId").alias("movie1"), \
+#         func.col("ratings2.movieId").alias("movie2"), \
+#         func.col("ratings1.rating").alias("rating1"), \
+#         func.col("ratings2.rating").alias("rating2"))
+moviePairs.show()
 
 moviePairSimilarities = computeCosineSimilarity(spark, moviePairs).cache()
 
@@ -96,14 +105,14 @@ if (len(sys.argv) > 1):
     # Sort by quality score.
     results = filteredResults.sort(func.col("score").desc()).take(10)
     
-    print ("Top 10 similar movies for " + getMovieName(movieNames, movieID))
+    # print ("Top 10 similar movies for " + getMovieName(movieNames, movieID))
     
-    for result in results:
-        # Display the similarity result that isn't the movie we're looking at
-        similarMovieID = result.movie1
-        if (similarMovieID == movieID):
-          similarMovieID = result.movie2
+    # for result in results:
+    #     # Display the similarity result that isn't the movie we're looking at
+    #     similarMovieID = result.movie1
+    #     if (similarMovieID == movieID):
+    #       similarMovieID = result.movie2
         
-        print(getMovieName(movieNames, similarMovieID) + "\tscore: " \
-              + str(result.score) + "\tstrength: " + str(result.numPairs))
+    #     print(getMovieName(movieNames, similarMovieID) + "\tscore: " \
+    #           + str(result.score) + "\tstrength: " + str(result.numPairs))
         
